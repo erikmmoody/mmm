c4dd2686c1886c78a02eb5efd91a093c3b3f0c13
erikmmoody <erikmmoody@gmail.com>
2020-02-20 22:40:26 +0000

    na

--- mmm/src/main/java/com/erik/mmm/MeanMedMode.java
+++ mmm/src/main/java/com/erik/mmm/MeanMedMode.java
@@ -4,18 +4,49 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.OptionalDouble;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 class MeanMedMode<N extends Number> {
+
+    public static <N extends Number> BigDecimal bigMean(Collection<N> list, int points) {
+        ArrayList<BigDecimal> bigList = new ArrayList<>();
+        for (N n : list) {
+            bigList.add(new BigDecimal(n.toString()));
+        }
+        BigDecimal total = bigList.stream().reduce(BigDecimal.ZERO, (a, b) -> a.add(b));
+        BigDecimal mean = total.divide(BigDecimal.valueOf(bigList.size()));
+        return mean.setScale(points, RoundingMode.HALF_UP);
+    }
 
     public static <N extends Number> double mean(Collection<N> list) {
         OptionalDouble average = list.stream().mapToDouble(n -> n.doubleValue()).average();
         return average.getAsDouble();
     }
-    
+
     public static <N extends Number> double median(Collection<N> list) {
-        ArrayList<N> nlist = new ArrayList<>(list);
+        ArrayList<Double> nlist = new ArrayList<>();
+        for (N n : list) {
+            nlist.add(n.doubleValue());
+        }
+        if (nlist.size() < 2) {
+            return nlist.get(0);
+        }
         int middle = nlist.size()/2;
-        return 2.0;
+        
+         
+        Collections.sort(nlist);
+        double median;
+        if (nlist.size() % 2 == 0) {
+            median = (nlist.get(middle) + nlist.get(middle-1))/2;
+        } else {
+            median = nlist.get(middle);
+
+        }
+        return median;
     }
 
 }
--- mmm/src/test/java/com/erik/mmm/AppTest.java
+++ mmm/src/test/java/com/erik/mmm/AppTest.java
@@ -2,8 +2,11 @@ package com.erik.mmm;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 /**
  * Unit test for simple App.
@@ -36,5 +39,40 @@ public class AppTest {
         input.add(7.0);
         double mean = MeanMedMode.mean(input);
         assertEquals(mean, 10.0/3.0, 0.0f);
+    }
+
+    @Test
+    void median() {
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        double median = MeanMedMode.median(input);
+        assertEquals(median, 3.0, 0.0f);
+    }
+
+    @Test
+    void medianEven() {
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        double median = MeanMedMode.median(input);
+        assertEquals(median, 3.5, 0.0f);
+    }
+
+    @Test
+    void bigMean() {
+        ArrayList<Double> input = new ArrayList<>(Arrays.asList(6.473343575064627, 25.074808132784444, 46.56863481297735, 
+                                                                42.15683212179127, 38.66108248577888, 83.03051130301404, 13.476309649766883, 
+                                                                54.80119585316232, 87.91815314181547, 71.15110877901036));
+        BigDecimal mean = MeanMedMode.bigMean(input, 14);
+        System.out.println("***********/*/*/*/**/*/*/*/*/*/*/");
+        System.out.println(mean);
+        assertEquals(mean.compareTo(BigDecimal.valueOf(46.93119798551656)), 0);
+
+    }
+
+    @Test
+    void bigMedian() {
+        ArrayList<Double> input = new ArrayList<>(Arrays.asList(6.473343575064627, 25.074808132784444, 46.56863481297735, 
+                                                                42.15683212179127, 38.66108248577888, 83.03051130301404, 13.476309649766883, 
+                                                                54.80119585316232, 87.91815314181547, 71.15110877901036));
+        double median = MeanMedMode.median(input);
+        assertEquals(median, 44.36273346738431, 0.0f);
     }
 }
